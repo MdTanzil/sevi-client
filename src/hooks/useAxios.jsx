9d3d2befdb1bf3f006a6802f05c85f39387a76b5
Md Tanzil <mtanzil1999@gmail.com>
2023-12-09 17:20:46 +0600

    readme update

--- src/hooks/useAxios.jsx
+++ src/hooks/useAxios.jsx
@@ -3,8 +3,8 @@ import axios from "axios";
 
 const useAxios = () => {
     const instance = axios.create({
-      baseURL: "https://sevi-server.vercel.app/api/v1",
-      //  baseURL: "http://localhost:5000/api/v1",
+      // baseURL: "https://sevi-server.vercel.app/api/v1",
+       baseURL: "http://localhost:5000/api/v1",
 
       withCredentials: true,
     });
